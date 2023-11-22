00f717e7f037df17926df27cd651f56ddb53cd1a
NeiroSlav <BodganSir1807@gmail.com>
2023-11-22 14:09:02 +0300

    server ip added

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from pages import *
 if __name__ == '__main__':
     token_thread.start()
     # app.run(debug=False, host='10.0.250.29', port=8000)  # мой им впн
-    app.run(debug=False, host='192.168.252.133', port=8000)  # ип сервера
+    # app.run(debug=False, host='192.168.252.133', port=8000)  # ип сервера
+    app.run(debug=True, host='192.168.252.133', port=8000)  # ип сервера
