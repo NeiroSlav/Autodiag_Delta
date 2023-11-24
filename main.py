25eb74d07ca9b5f79be27f9e6de4250df8caae0a
NeiroSlav <BogdanSir1807@gmail.com>
2023-11-24 03:39:12 +0300

    -

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     token_thread.start()
     # app.run(debug=True, port=8000)  # локалхост
     # app.run(debug=False, host='10.0.250.29', port=8000)  # мой впн
-    app.run(debug=True, host='192.168.252.133', port=8000)  # ип сервера
+    app.run(debug=False, host='192.168.252.133', port=8000)  # ип сервера
