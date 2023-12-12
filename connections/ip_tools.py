6e4057d0d8e9b4ef7088ee0651c67885de72b3a1
NeiroSlav <BogdanSir1807@gmail.com>
2023-12-12 21:31:55 +0300

    +

--- connections/ip_tools.py
+++ connections/ip_tools.py
@@ -5,25 +5,25 @@ import subprocess
 
 #  пинг утилитой fping (очень быстрый)
 def fping(ip_address: str) -> bool:
-    res = os.system(f"fping -c1 -t500 {ip_address}")
-    print('res =', res)
-    if res != 0:  # если один пакет не прошёл, пробует ещё два с таймаутом 1сек
-        res = os.system(f"fping -c2 -t1000 {ip_address}")
-        if res != 0:
-            return False
+    # res = os.system(f"fping -c1 -t500 {ip_address}")
+    # print('res =', res)
+    # if res != 0:  # если один пакет не прошёл, пробует ещё два с таймаутом 1сек
+    #     res = os.system(f"fping -c2 -t1000 {ip_address}")
+    #     if res != 0:
+    #         return False
     return True
 
 
 def nmap(ip_address: str) -> dict:
-    command = f"nmap -PN -p 80,8080,90,9090,1080,8000,9000,666,8888,9091,4978 {ip_address}"
-    answer = subprocess.check_output(command, shell=True)
-    answer = str(answer).replace('\\n', '\n')
+    # command = f"nmap -PN -p 80,8080,90,9090,1080,8000,9000,666,8888,9091,4978 {ip_address}"
+    # answer = subprocess.check_output(command, shell=True)
+    # answer = str(answer).replace('\\n', '\n')
+    #
+    # port = re.search(r'[0-9]+/tcp +open', answer)  # ищет открытый порт
+    # if not port:  # если порт не найден
+    #     return {'ip': ip_address, 'port': None}
+    #
+    # port = port.group(0).split('/')[0]
+    # return {'ip': ip_address, 'port': port}
 
-    port = re.search(r'[0-9]+/tcp +open', answer)  # ищет открытый порт
-    if not port:  # если порт не найден
-        return {'ip': ip_address, 'port': None}
-
-    port = port.group(0).split('/')[0]
-    return {'ip': ip_address, 'port': port}
-
-    # return {'ip': ip_address, 'port': '9090'}
+    return {'ip': ip_address, 'port': '9090'}
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from pages import *
 if __name__ == '__main__':
     token_thread.start()
 
-    # app.run(debug=True, port=8000)  # локалхост
+    app.run(debug=True, port=8000)  # локалхост
 
     app.run(debug=False, host='192.168.252.133', port=8000)  # ип сервера
