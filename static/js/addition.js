53dbcb300ebc28042e61e8820130de64e8a3569b
NeiroSlav <BodganSir1807@gmail.com>
2023-12-12 11:51:43 +0300

    front

--- static/js/addition.js
+++ static/js/addition.js
@@ -12,12 +12,21 @@ var additionShownFlag = false
 
 function showAddition() {
     // console.log(responce);
-    var oldDiv = document.getElementById('additionPanel');  // Достаём старый div
+    var additionPanel = document.getElementById('additionPanel');  // Достаём div
+    var sepRight = document.getElementById('sepRight');
+    var mainPanel = document.getElementById('mainPanel');
+
 
     if (additionShownFlag) { //  если панель показана, то скрыть
-        oldDiv.style.width = 0 + "px";
+        additionPanel.style.width = 0;
+        sepRight.style.width = 0;
+        mainPanel.style.padding = "0 48px 0 48px";
+
     } else {                 //  если панель скрыта, то показать
-        oldDiv.style.width = 272 + "px";
+        additionPanel.style.width = 272 + "px";
+        sepRight.style.width = 24 + "px";
+        mainPanel.style.padding = "0 12px 0 12px";
+
     }
 
     additionShownFlag = !additionShownFlag
